76f1e5419248693c6ce1a301bf5e0e55d2cf3f5b
coolwebgru <rolandschurz198813@gmx.de>
2019-03-01 15:22:47 +0700

    commit for testing

---
+++ controllers/CronController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\helpers\VarDumper;
+use app\models\UserAnswers;
+use app\models\EventStatus;
+use app\models\PricingFormula;
+use app\models\FixedValues;
+use app\models\Token;
+
+class CronController extends Controller
+{
+    public $layout = 'questions';
+
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => \yii\filters\VerbFilter::className(),
+                'actions' => [
+                    'formula_prices' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    public function beforeAction($action)
+    {
+        $this->enableCsrfValidation = false;
+        return parent::beforeAction($action);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex() {
+        $token = new TOken();
+        $token->access_token = 'test';
+        $token->refresh_token = 'test';
+        $token->save();
+    }
+//
+//    public function run() {
+//        $token = new TOken();
+//        $token->access_token = 'test';
+//        $token->refresh_token = 'test';
+//        $token->save();
+//    }
+}
